f0d2d74bcf76e46732134e6b24b49884679640fc
Ian Olsen <ian.olsen@gmail.com>
2009-12-24 10:54:24 -0600

    Documentation updates. Changed license back to GNU Affero.
    Update issue 2
    Status: Started
    Still need to update linux install/upgrade

--- org/dazeend/harmonium/screens/AboutScreen.java
+++ org/dazeend/harmonium/screens/AboutScreen.java
@@ -4,7 +4,7 @@
  * This file is part of Harmonium, the TiVo music player.
  *
  * Harmonium is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as 
+ * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
@@ -17,7 +17,7 @@
  * License along with Harmonium.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
- 
+
 package org.dazeend.harmonium.screens;
 
 import org.dazeend.harmonium.screens.HScrollPane;
@@ -39,16 +39,17 @@ public class AboutScreen extends HScreen {
 	private static final String OK_LABEL = "Return to Main Menu";
 	private String message;
 	private HScrollPane scrollPane;
-	
+
 	/**
 	 * @param app
 	 * @param title
 	 */
 	public AboutScreen(Harmonium app) {
-		super(app, "About Harmonium");	
-		
+		super(app, "About Harmonium");
+
 		this.app = app;
-		this.message = "Harmonium Tivo Music Player\nVersion: " + HarmoniumFactory.getVersion() + "\nCopyright (C) 2008-2009  Charles Perry, Ian Olsen\n\nHarmonium is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+		this.message = "Harmonium Music Player\nVersion: " + HarmoniumFactory.getVersion() + "\nCopyright (C) 2008-2009  Charles Perry, Ian Olsen\n\nHarmonium is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.\n\nYou should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\nIf you would like the source code for Harmonium, you can download it from <http://code.google.com/p/harmonium/>.";
+
 		// Create text message view
 		this.scrollPane = new HScrollPane(	this.getNormal(),
 											this.safeTitleH,
@@ -56,9 +57,9 @@ public class AboutScreen extends HScreen {
 											this.screenWidth - (2 * this.safeTitleH),
 											this.rowHeight * 6
 		);
-		
-		
-		BText messageText = new BText(	this.scrollPane, 
+
+
+		BText messageText = new BText(	this.scrollPane,
 										0,
 										0,
 										this.scrollPane.getWidth(),
@@ -70,10 +71,10 @@ public class AboutScreen extends HScreen {
 		messageText.setValue(this.message);
 		messageText.setVisible(true);
 		setManagedView(messageText);
-		
+
 		// Refresh the scroll pane
 		this.scrollPane.refresh();
-		
+
 		// Create OK button
 		this.OKButton = new BButton(this.getNormal(),										// Put list on "normal" level
 									this.safeTitleH,										// x coord. of button
@@ -84,25 +85,25 @@ public class AboutScreen extends HScreen {
 		setManagedResource(OKButton, createText(this.app.hSkin.barFont, HSkin.NTSC_WHITE, OK_LABEL));
 		this.OKButton.setBarAndArrows(BAR_HANG, BAR_DEFAULT, "pop", null, null, null, true);
 		this.OKButton.setFocusable(true);
-		
+
 		// Set default focus on scrollPane
 		this.setFocusDefault(this.OKButton);
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see com.tivo.hme.bananas.BView#handleKeyPress(int, long)
 	 */
 	@Override
 	public boolean handleKeyPress(int code, long rawcode) {
-		
+
 		this.app.checkKeyPressToResetInactivityTimer(code);
-		
+
 		if(this.getFocus() != null && this.getFocus().equals(this.OKButton) && code == KEY_SELECT) {
-			
+
 			this.app.pop();
 			return true;
 		}
-		
+
 		  switch (code) {
 	        case KEY_UP:
 	            this.scrollPane.lineUp();
